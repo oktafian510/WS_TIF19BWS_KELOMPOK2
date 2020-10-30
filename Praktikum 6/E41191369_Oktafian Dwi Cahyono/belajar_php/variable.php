5c736fb4357331d827a9f46af4f8e5a624c6a32c
E41191369_Oktafian Dwi Cahyono <71855139+oktafian510@users.noreply.github.com>
2020-10-30 22:56:30 +0700

    hasil percobaan

---
+++ Praktikum 6/E41191369_Oktafian Dwi Cahyono/belajar_php/variable.php
@@ -0,0 +1,9 @@
+<?php
+// membuat  Variable baru
+$nama = "OKta";
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+
+// menampilkan variable
+echo "$nama Membeli   $nama_barang   Seharga   $harga";
